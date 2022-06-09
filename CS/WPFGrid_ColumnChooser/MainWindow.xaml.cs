1f6297ceb990fe47799187b6f8c815b9ae099d48
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2022-06-09 15:25:16 +0400

    Add files via upload

---
+++ CS/WPFGrid_ColumnChooser/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Xpf.Grid;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFGrid_ColumnChooser {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            ObservableCollection<Item> items = new ObservableCollection<Item>();
+            for (int i = 1; i < 30; i++) {
+                items.Add(new Item() { Id = i, Name = "Name" + i });
+            }
+            grid.ItemsSource = items;
+            view.ShowColumnChooser();
+        }
+    }
+    public class HeaderTemplateSelector : DataTemplateSelector {
+        public DataTemplate ColumnChooserTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            if (ColumnBase.GetHeaderPresenterType(container) == HeaderPresenterType.ColumnChooser && (string)item == nameof(Item.Id))
+                return ColumnChooserTemplate;
+            return base.SelectTemplate(item, container);
+        }
+    }
+    public class Item {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
