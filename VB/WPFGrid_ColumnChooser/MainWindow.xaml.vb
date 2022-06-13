65bb0f8f835ee5ee47052ece461b793a4ebcb1b0
DevExpressExampleBot <fakeEmail>
2022-06-13 12:15:13 +0400

    autogenerate VB

---
+++ VB/WPFGrid_ColumnChooser/MainWindow.xaml.vb
@@ -0,0 +1,40 @@
+Imports DevExpress.Xpf.Grid
+Imports System.Collections.ObjectModel
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace WPFGrid_ColumnChooser
+
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim items As ObservableCollection(Of Item) = New ObservableCollection(Of Item)()
+            For i As Integer = 1 To 30 - 1
+                items.Add(New Item() With {.Id = i, .Name = "Name" & i})
+            Next
+
+            Me.grid.ItemsSource = items
+            Me.view.ShowColumnChooser()
+        End Sub
+    End Class
+
+    Public Class HeaderTemplateSelector
+        Inherits DataTemplateSelector
+
+        Public Property ColumnChooserTemplate As DataTemplate
+
+        Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+            If ColumnBase.GetHeaderPresenterType(container) = HeaderPresenterType.ColumnChooser AndAlso Equals(CStr(item), NameOf(WPFGrid_ColumnChooser.Item.Id)) Then Return ColumnChooserTemplate
+            Return MyBase.SelectTemplate(item, container)
+        End Function
+    End Class
+
+    Public Class Item
+
+        Public Property Id As Integer
+
+        Public Property Name As String
+    End Class
+End Namespace
